c3bd513a9df7b96cf086b305c7e61ce38d2d96da
ArthurDelamare <arthur.delamare1@viacesi.fr>
2016-12-13 10:05:15 +0100

    Maj

--- EXIASAVER_HOME/exiasaver2.c
+++ EXIASAVER_HOME/exiasaver2.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "BibliExiaSaver2.h"
 
 int main(int argc, char* argv[], char** envp)
 {
+  int pid;
+  int i;
+  
   printf("Je suis le programme 2\n");
   printf("la taille des lettres est: %s\n", getenv("EXIASAVER2_TAILLE"));
-  heurePBM();
-  execl("Afficheur","EXIASAVER2_PBM/combinaison.pbm",NULL);
+  while(1)
+    {
+      heurePBM();
+      pid = fork();
+      if (pid==0)
+	{
+	  execl("Afficheur","EXIASAVER2_PBM/combinaison.pbm",NULL);
+	}
+      printf("\nl'image s'actualisera dans %s secondes",getenv("EXIASAVER2_SLEEP"));
+      wait(NULL);
+      for (i=0;i<10;i++)
+	{
+	  printf(".\n");
+	  sleep(1);
+	}
+    }
   return (0);
 }

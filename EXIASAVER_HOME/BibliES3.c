8f5a827427736fe54e195622059c26c1e871b68e
ArthurDelamare <arthur.delamare1@viacesi.fr>
2016-12-14 11:43:07 +0100

    ajout debut interactif

---
+++ EXIASAVER_HOME/BibliES3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void initTableau(int tab[][80])
+{
+  int i,j;
+  for (j=0;j<23;j++)
+    {
+      for (i=0;i<80;i++)
+	{
+	  tab[j][i]=0;
+	}
+    }
+}
+
+void moveDroite(int x,int y,int tab[][80])
+{
+  //Rangée centrale de l'avion
+  tab[y][x]=1;
+  tab[y][x+1]=1;
+  tab[y][x+2]=1;
+  tab[y][x-1]=1;
+  tab[y][x-2]=1;
+  tab[y][x-3]=1;
+
+  //Queue de l'avion
+  tab[y-1][x-3]=1;
+
+  //Ailes de l'avion
+  tab[y-1][x+1]=1;
+  tab[y-2][x]=1;
+  tab[y+1][x+1]=1;
+  tab[y+2][x]=1;
+  
+}
--- EXIASAVER_HOME/exiasaver3.c
+++ EXIASAVER_HOME/exiasaver3.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "BibliES3.h"
 
 int main(int argc, char* argv[], char** envp)
 {
-  printf("Je suis le programme 3\n");
-  printf("la position iniatiale de l'avion est: %s\n", argv[0]);
+  //variables qui détermine la position du point central de l'avion
+  int x=40;
+  int y=11;
+  //tableau à deux dimensions définissant le terrain dans lequel l'avion se déplace
+  int tabTerrain[23][80] = {0};
 
+  int i,j;
+  
+  initTableau(tabTerrain);
+  moveDroite(x,y,tabTerrain);
+  
+  // printf("Je suis le programme 3\n");
+  // printf("la position iniatiale de l'avion est: %s\n", argv[0]);
+
+  for(j=0;j<23;j++)
+    {
+      for(i=0;i<80;i++)
+	{
+	  printf("%d",tabTerrain[j][i]);
+	}
+    }
 
   return (0);
 }

da7e0facfbad02468a93838f3f8118d77d486f83
ArthurDelamare <arthur.delamare1@viacesi.fr>
2016-12-15 11:06:45 +0100

    correctif lanceur

---
+++ LanceurES.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include "BibliLanceurES.h"
+
+int main(int argc, char* argv[],char** envp)
+{
+  if (argc == 2 && strcmp(argv[1],"-config") == 0)
+    {
+      //Changement des variables d'environnement par l'utilisateur
+      choixVarEnv();
+    }
+    
+  //Déclaration de variables utilisées dans ce programme
+  int nbProgramme = 0; //variable stockant le numero du programme a executé
+  int pid; //variable permettant de savoir s'il s'agit du proccesus père ou fils
+  int fichierChoisi;
+  char chemin[256]="";
+  char adresseExe[100];
+  
+  system("clear");
+  if ((argc == 1) || (strcmp(argv[1],"-config") == 0) )
+    {
+      //détermination aléatoire du programme a lancé
+      srand(time(NULL));
+      nbProgramme = (rand() % 3) + 1;
+      printf("le programme à lancer est le numero %d\n", nbProgramme);
+      pid = fork();
+      if (pid == 0)
+	{
+	  if (nbProgramme == 1)
+	    {
+	      fichierChoisi = (rand() % 5) + 1 ;
+	      printf("le fichier a affiche est le %d\n", fichierChoisi);
+	      choixFichier(fichierChoisi,chemin);
+	      strcpy(chemin,strcat(getenv("EXIASAVER1_PBM"),chemin));
+	      printf("Salut %s\n",chemin);
+	      historique(1,chemin,"");
+	      strcpy(adresseExe,getenv("EXIASAVER_HOME"));
+	      execl(strcat(adresseExe,"/exiasaver1"),chemin,NULL);
+	      exit(1);
+	    }
+	  else if (nbProgramme == 2)
+	    {
+	      historique(2,"","");
+	      strcpy(adresseExe,getenv("EXIASAVER_HOME"));
+	      execl(strcat(adresseExe,"/exiasaver2"),"",NULL);
+	      exit(2);
+	    }
+	  else if (nbProgramme == 3)
+	    {
+	      historique(3,"","9x10");
+	      strcpy(adresseExe,getenv("EXIASAVER_HOME"));
+	      execl(strcat(adresseExe,"/exiasaver3"),"9x10",NULL);
+	      exit(3);
+	    }
+	}
+      else
+	{
+	  wait(NULL);
+	}
+    }
+  else if(argc == 2 && strcmp(argv[1],"-stats") == 0)
+    {
+      statistique();
+    }
+  else
+    {
+      printf("les arguments sont incorrect\n");
+    }
+  return (0);
+}

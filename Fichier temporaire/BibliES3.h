ba19031091f45eeb75b08465d9484693769770c9
ArthurDelamare <arthur.delamare1@viacesi.fr>
2016-12-15 11:05:18 +0100

    tri arborescence

--- Fichier temporaire/BibliES3.h
+++
@@ -1,6 +0,0 @@
-void initTableau(int tab[23][60]);
-void moveDroite(int x, int y, int tab[23][60]);
-void moveGauche(int x, int y, int tab[23][60]);
-void moveHaut(int x, int y, int tab[23][60]);
-void moveBas(int x, int y, int tab[23][60]);
-void printTabPBM(int tab[23][60]);
--- Fichier temporaire/BibliExiaSaver2.h
+++
@@ -1,2 +0,0 @@
-void analyse(char portion, char* conteneurChemin);
-void heurePBM();
